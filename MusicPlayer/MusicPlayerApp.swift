ee896d575d251f02e1a46419d1d8ef5243ef687b
marcosocon <oconmarcos97@gmail.com>
2020-10-15 19:48:34 -0300

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Marcos Ocon on 15/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
